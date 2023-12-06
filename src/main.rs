e85a546c3a97963a8fae8c9e39609587b97c2380
lisa crossman <lisacrossman@lisas-MacBook-Pro-2.local>
2023-12-06 09:15:39 +0000

    fixing bug

--- src/main.rs
+++ src/main.rs
@@ -81,7 +81,7 @@ struct Pcrrequest<'f> {
 pub struct RateLimitGuard;
 
 impl<'r> RocketGovernable<'r> for RateLimitGuard {
-    fn quota(_method: rocket_governor::Method, _route_name: &str) -> Quota {
+    fn quota(_method: in_silico_pcr_rocket_governor::Method, _route_name: &str) -> Quota {
         Quota::per_second(Self::nonzero(1u32))
     }
 }
